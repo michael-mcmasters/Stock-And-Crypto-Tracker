cfd716090eb93145b710918a5119f41b2da8ab9b
Michael McMasters <jmcmasters411@gmail.com>
2021-07-05 23:14:01 -0400

    Create CoinGallery component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import styled from "styled-components";
-import Coin from "./components/Coin";
-import './App.css';
+import CoinGallery from "./components/CoinGallery";
 
 function App() {
   return (
-    <Container>
-      <Coin coinTicker={"BTC"} price={"$33,786.51"} />
-      <Coin coinTicker={"ETH"} price={"$2,221.79"} />
-      <Coin coinTicker={"DOGE"} price={"$0.234209"} />
-    </Container>
+    <CoinGallery />
   );
 }
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-around;
-  border: 1px solid red;
-  width: 70%;
-  margin: 3em auto 0 auto;
-`;
 
 export default App;
---
+++ src/components/CoinGallery.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import Coin from "./Coin";
+
+function CoinGallery() {
+  return (
+    <Container>
+      <Coin coinTicker={"BTC"} price={"$33,786.51"} />
+      <Coin coinTicker={"ETH"} price={"$2,221.79"} />
+      <Coin coinTicker={"DOGE"} price={"$0.234209"} />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  border: 1px solid red;
+  width: 70%;
+  margin: 3em auto 0 auto;
+`;
+
+export default CoinGallery;
