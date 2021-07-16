af55ce9986ea2813e9c0913015c58b214c1b8638
Michael McMasters <jmcmasters411@gmail.com>
2021-07-15 23:38:07 -0400

    Add percentage to tickers

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -11,28 +11,28 @@ const Ticker = ({ tickerObj, getTickerFontAndBGColors }) => {
   return (
     <Container colors={colors} fontColor={fontColor} bgColor={bgColor}>
       <CoinTicker>{tickerName}</CoinTicker>
-      <Price>${price}</Price>
+      <Price>${price} (0.0%)</Price>
     </Container>
   );
 };
 
 const Container = styled.div`
-  margin: 1em 1em;
-  padding: 1em 0;
-  border: 1px solid ${props => props.fontColor};
-  border-radius: 10px;
-  width: 10em;
-  color: ${props => props.fontColor};
-  background-color: ${props => props.bgColor};
-  text-align: center;
-`;
+      margin: 1em 1em;
+      padding: 1em 0;
+      border: 1px solid ${props => props.fontColor};
+      border-radius: 10px;
+      width: 10em;
+      color: ${props => props.fontColor};
+      background-color: ${props => props.bgColor};
+      text-align: center;
+      `;
 
 const CoinTicker = styled.div`
-  font-weight: bold;
-`;
+      font-weight: bold;
+      `;
 
 const Price = styled.div`
-  margin-top: 0.5em;
-`;
+      margin-top: 0.5em;
+      `;
 
 export default Ticker;
