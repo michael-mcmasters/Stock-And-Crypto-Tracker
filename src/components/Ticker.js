bc42f58880f8b9200f5bfc1e3d089caade6f2fc4
Michael McMasters <jmcmasters411@gmail.com>
2021-07-17 20:36:49 -0400

    Move Ticker's visual logic into component

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,38 +1,51 @@
 import styled from "styled-components";
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { ColorThemeContext } from "./custom_hooks/ColorThemeContext";
 
-const Ticker = ({ tickerName, type, price, prevPrice, priceDifference, percentage, getTickerFontAndBGColors }) => {
+const Ticker = ({ tickerName, type, price, prevPrice, priceDifference, percentage }) => {
+  const COLORS = useContext(ColorThemeContext);
 
-  const colors = useContext(ColorThemeContext);
-  const [bgColor, fontColor] = getTickerFontAndBGColors(price, prevPrice);
+  const getFontAndBGColor = (price, prevPrice) => {
+    if (price < prevPrice) return [COLORS.brightRed, COLORS.darkRed];
+    else return [COLORS.green, COLORS.basicGreen];
+  };
+
+  const formatPriceDifference = (priceDifference) => {
+    if (priceDifference < 0) return priceDifference;
+    else return "+" + priceDifference;
+  };
+
+  const [bgColor, fontColor] = getFontAndBGColor(price, prevPrice);
+  priceDifference = formatPriceDifference(priceDifference);
 
   return (
-    <Container colors={colors} fontColor={fontColor} bgColor={bgColor}>
+    <Container colors={COLORS} fontColor={fontColor} bgColor={bgColor}>
       <CoinTicker>{tickerName}</CoinTicker>
       <Price>${price}</Price>
-      <div>{priceDifference} ({percentage}%)</div>
+      <div>
+        {priceDifference} ({percentage}%)
+      </div>
     </Container>
   );
 };
 
 const Container = styled.div`
-      margin: 1em 1em;
-      padding: 1em 0;
-      border: 1px solid ${props => props.fontColor};
-      border-radius: 10px;
-      width: 10em;
-      color: ${props => props.fontColor};
-      background-color: ${props => props.bgColor};
-      text-align: center;
+  margin: 1em 1em;
+  padding: 1em 0;
+  border: 1px solid ${(props) => props.fontColor};
+  border-radius: 10px;
+  width: 10em;
+  color: ${(props) => props.fontColor};
+  background-color: ${(props) => props.bgColor};
+  text-align: center;
 `;
 
 const CoinTicker = styled.div`
-      font-weight: bold;
+  font-weight: bold;
 `;
 
 const Price = styled.div`
-      margin-top: 0.5em;
+  margin-top: 0.5em;
 `;
 
 export default Ticker;
--- src/components/TickerGallery.js
+++ src/components/TickerGallery.js
@@ -6,12 +6,10 @@ import AddTickerInputField from "./AddTickerInputField";
 
 const MAX_ALLOWED_TICKERS = 16;
 // const PRICE_UPDATE_DELAY = 5000;    // 5 seconds
-const PRICE_UPDATE_DELAY = 100000;
+const PRICE_UPDATE_DELAY = 5000;
 const DEBUG_USE_FAKE_PRICES = false;
 
-
 function TickerGallery() {
-
   const colors = useContext(ColorThemeContext);
   const [tickersArr, setTickersArr] = useState(getTickerObjects());
 
@@ -23,41 +21,31 @@ function TickerGallery() {
         arr[i].currentPrice = (Math.random() * 10).toFixed(6);
       }
       setTickersArr(arr);
-
     } else {
       let arr = [...tickersArr];
       for (let i = 0; i < arr.length; i++) {
         // Example URI: http://localhost:8080/stock/botz
         fetch(`http://localhost:8080/${arr[i].type}/${arr[i].tickerName}`)
-          .then(res => res.json())
-          .then(res => {
-            if (res.day == null)
-              console.log(res);
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.day == null) console.log(res);
             arr[i].prevPrice = tickersArr[i].currentPrice;
             arr[i].currentPrice = res.currentPrice;
-            arr[i].day.priceDifference = res.day?.priceDifference;
-            arr[i].day.percentage = res.day?.percentage.toFixed(2);
+            arr[i].priceDifference = res.day?.priceDifference;
+            arr[i].percentage = res.day?.percentage.toFixed(2);
           });
       }
       setTickersArr(arr);
     }
-  }
-
-  const getTickerFontAndBGColors = (price, prevPrice) => {
-    if (price > prevPrice) {
-      return [colors.green, colors.basicGreen];
-    }
-    return [colors.brightRed, colors.darkRed];
-  }
+  };
 
   const handleAddTicker = (tickerName, type) => {
-    if (tickersArr.length >= MAX_ALLOWED_TICKERS)
-      return;
+    if (tickersArr.length >= MAX_ALLOWED_TICKERS) return;
 
     let arr = [...tickersArr];
     arr.push({ tickerName, type: type, currentPrice: 0, prevPrice: 0 });
     setTickersArr(arr);
-  }
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -65,7 +53,7 @@ function TickerGallery() {
     }, PRICE_UPDATE_DELAY);
 
     return () => clearInterval(interval);
-  })
+  });
 
   return (
     <>
@@ -85,9 +73,7 @@ function TickerGallery() {
         {/* <Head>
           Today | Week | Month | 6 Months | YTD | Year
         </Head> */}
-        <Head>
-          Today | Week | Month | YTD | Year
-        </Head>
+        <Head>Today | Week | Month | YTD | Year</Head>
         <GridContainer>
           {tickersArr.map((t, keyIndex) => (
             <Ticker
@@ -96,11 +82,10 @@ function TickerGallery() {
               type={t.type}
               price={t.currentPrice}
               prevPrice={t.prevPrice}
-              priceDifference={t.day.priceDifference}
-              percentage={t.day.percentage}
-              getTickerFontAndBGColors={getTickerFontAndBGColors}
-            />)
-          )}
+              priceDifference={t.priceDifference}
+              percentage={t.percentage}
+            />
+          ))}
         </GridContainer>
         <AddTickerInputField handleAddTicker={handleAddTicker} />
       </Container>
@@ -128,13 +113,12 @@ const GridContainer = styled.div`
     grid-template-columns: repeat(3, auto);
     grid-template-rows: repeat(3, auto);
   }
-  
+
   @media (max-width: 650px) {
     grid-template-columns: repeat(2, auto);
     grid-template-rows: repeat(2, auto);
   }
 `;
-
 
 function getTickerObjects() {
   return [
@@ -143,122 +127,98 @@ function getTickerObjects() {
       type: "crypto",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "ETH",
       type: "crypto",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "TWTR",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "YOLO",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "DOGE",
       type: "crypto",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "BOTZ",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "AAPL",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "AMZN",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "GOOGL",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "TSLA",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "VTSAX",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
     {
       tickerName: "UBER",
       type: "stock",
       currentPrice: 0,
       prevPrice: 0,
-      day: {
-        priceDifference: 0,
-        percentage: 0.0
-      }
+      priceDifference: 0,
+      percentage: 0.0,
     },
-  ]
+  ];
 }
 
 export default TickerGallery;
