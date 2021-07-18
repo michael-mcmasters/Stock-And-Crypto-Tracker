6320209ce28cd391768ebebdae213990e3cfbef3
Michael McMasters <jmcmasters411@gmail.com>
2021-07-17 21:16:30 -0400

    Clean up imports and delete commented out code

--- src/components/TickerGallery.js
+++ src/components/TickerGallery.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, useContext } from "react";
-import { ColorThemeContext } from "./custom_hooks/ColorThemeContext";
 import styled from "styled-components";
 import Ticker from "./Ticker";
 import AddTickerInputField from "./AddTickerInputField";
@@ -9,19 +8,16 @@ const PRICE_UPDATE_DELAY = 30000; // 5000 is 5 seconds
 const DEBUG_USE_FAKE_PRICES = false;
 
 function TickerGallery() {
-  const colors = useContext(ColorThemeContext);
   const [tickersArr, setTickersArr] = useState(getTickerObjects());
 
   const updatePrices = async () => {
+    let arr = [...tickersArr];
     if (DEBUG_USE_FAKE_PRICES) {
-      let arr = [...tickersArr];
       for (let i = 0; i < arr.length; i++) {
         arr[i].prevPrice = tickersArr[i].currentPrice;
         arr[i].currentPrice = (Math.random() * 10).toFixed(6);
       }
-      setTickersArr(arr);
     } else {
-      let arr = [...tickersArr];
       for (let i = 0; i < arr.length; i++) {
         // Example URI: http://localhost:8080/stock/botz
         let res = await fetch(`http://localhost:8080/${arr[i].type}/${arr[i].tickerName}`);
@@ -32,8 +28,8 @@ function TickerGallery() {
         arr[i].priceDifference = res.day?.priceDifference;
         arr[i].percentage = res.day?.percentage.toFixed(2);
       }
-      setTickersArr(arr);
     }
+    setTickersArr(arr);
   };
 
   const handleAddTicker = (tickerName, type) => {
@@ -55,21 +51,6 @@ function TickerGallery() {
   return (
     <>
       <Container>
-        {/* <Head>
-          Today | Week | Month | Year | All Time
-        </Head> */}
-        {/* <Head>
-          1 DAY | 5 DAY | 1 MONTH | 3 MONTH | 1 YEAR | YTD
-        </Head> */}
-        {/* <Head>
-          {"1 DAY | 5 DAY | 1 MONTH | 3 MONTH | 1 YEAR | YTD".toLowerCase()}
-        </Head> */}
-        {/* <Head>
-          1D | 5D | 1M | 3M | 1Y | YTD
-        </Head> */}
-        {/* <Head>
-          Today | Week | Month | 6 Months | YTD | Year
-        </Head> */}
         <Head>Today | Week | Month | YTD | Year</Head>
         <GridContainer>
           {tickersArr.map((t, keyIndex) => (
