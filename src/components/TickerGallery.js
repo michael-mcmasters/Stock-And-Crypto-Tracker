5ab916a7f9adbda00ad1830485368141c15bd592
Michael McMasters <jmcmasters411@gmail.com>
2021-07-25 15:08:18 -0400

    TickersArr is now assigned to the entire response object instead of part of it. Can see price changes when pressing history buttons

--- src/components/TickerGallery.js
+++ src/components/TickerGallery.js
@@ -5,56 +5,43 @@ import AddTickerInputField from "./AddTickerInputField";
 import HistoryOptionsGallery from "./HistoryOptionsGallery";
 import HistoryOptions from "../constants/HistoryOptions";
 
-const DEBUG_USE_FAKE_PRICES = false;
+const DEBUG_USE_FAKE_PRICES = true;
 const MAX_ALLOWED_TICKERS = 16;
 const PRICE_UPDATE_DELAY = 15000; // 5000 is 5 seconds
 
 function TickerGallery() {
   const [tickersArr, setTickersArr] = useState(getTickerObjects());
   const [selectedHistoryOption, setSelectedHistoryOption] = useState(HistoryOptions.DAY);
-  const [response, setResponse] = useState([]);
 
   const handleClickHistoryOption = (historyOption) => {
     setSelectedHistoryOption(historyOption);
-    update(historyOption);
-    console.log(historyOption);
   }
-
-  const update = (historyOption) => {
-    let arr = [...tickersArr];
-    for (let i = 0; i < arr.length; i++) {
-      arr[i].currentPrice = response[i].currentPrice;
-      arr[i].priceDifference = response[i][selectedHistoryOption]?.priceDifference;
-      arr[i].percentage = response[i][selectedHistoryOption]?.percentage.toFixed(2);
-    }
-    setTickersArr(arr);
-  }
-
-  console.log("Re-render. selectedHistoryOption is: " + selectedHistoryOption);
 
   const updatePrices = async () => {
     let arr = [...tickersArr];
-    // let response = [];
-    if (DEBUG_USE_FAKE_PRICES) {
+    if (!DEBUG_USE_FAKE_PRICES) {
+      for (let i = 0; i < arr.length; i++) {
+        // Example URI: http://localhost:8080/stock/botz
+        let response = await fetch(`http://localhost:8080/${arr[i].type}/${arr[i].tickerName}`);
+        response = await response.json();
+        arr[i] = response;
+      }
+    } else {
       for (let i = 0; i < arr.length; i++) {
         const prevPrice = tickersArr[i].currentPrice;
         arr[i].currentPrice = (Math.random() * 10).toFixed(6);
-        arr[i].priceDifference = (arr[i].currentPrice - prevPrice).toFixed(2);
-        arr[i].percentage = (Math.random() * 4).toFixed(2);
-      }
-    } else {
-      console.log("updatePrices: selectedHistoryOption is: " + selectedHistoryOption);
-      for (let i = 0; i < arr.length; i++) {
-        // Example URI: http://localhost:8080/stock/botz
-        let res = await fetch(`http://localhost:8080/${arr[i].type}/${arr[i].tickerName}`);
-        res = await res.json();
-        if (res.day == null) console.log(res);
-        arr[i].currentPrice = res.currentPrice;
-        arr[i].priceDifference = res[selectedHistoryOption]?.priceDifference;
-        arr[i].percentage = res[selectedHistoryOption]?.percentage.toFixed(2);
+        arr[i].day.priceDifference = (arr[i].currentPrice - prevPrice).toFixed(2);    // priceDifference and percentage are not mathematically correct. These numbers are just for visualizing.
+        arr[i].day.percentage = (Math.random() * 4).toFixed(2);
+        arr[i].week.priceDifference = (Math.random() * 4 - prevPrice).toFixed(2);
+        arr[i].week.percentage = (Math.random() * 5).toFixed(2);
+        arr[i].month.priceDifference = (Math.random() * 4 - prevPrice).toFixed(2);
+        arr[i].month.percentage = (Math.random() * 6).toFixed(2);
+        arr[i].ytd.priceDifference = (Math.random() * 4 - prevPrice).toFixed(2);
+        arr[i].ytd.percentage = (Math.random() * 7).toFixed(2);
+        arr[i].year.priceDifference = (Math.random() * 4 - prevPrice).toFixed(2);
+        arr[i].year.percentage = (Math.random() * 8).toFixed(2);
       }
     }
-    setResponse(arr);
     setTickersArr(arr);
   };
 
@@ -68,12 +55,11 @@ function TickerGallery() {
 
   useEffect(() => {
     updatePrices();
-
     const interval = setInterval(() => {
       updatePrices();
     }, PRICE_UPDATE_DELAY);
     return () => clearInterval(interval);
-  }, [selectedHistoryOption]);
+  }, []);
 
   return (
     <>
@@ -86,8 +72,8 @@ function TickerGallery() {
               tickerName={t.tickerName}
               type={t.type}
               price={t.currentPrice}
-              priceDifference={t.priceDifference}
-              percentage={t.percentage}
+              priceDifference={t[selectedHistoryOption].priceDifference}
+              percentage={t[selectedHistoryOption].percentage}
             />
           ))}
         </GridContainer>
@@ -124,71 +110,251 @@ function getTickerObjects() {
       tickerName: "TWTR",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "AAPL",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "BOTZ",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "AMZN",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "BTC",
       type: "crypto",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "ETH",
       type: "crypto",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "DOGE",
       type: "crypto",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "YOLO",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "GOOGL",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "TSLA",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     // {
     //   tickerName: "VTSAX",
@@ -201,15 +367,51 @@ function getTickerObjects() {
       tickerName: "UBER",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
     {
       tickerName: "LYFT",
       type: "stock",
       currentPrice: 0,
-      priceDifference: 0,
-      percentage: 0.0,
+      day: {
+        priceDifference: 0,
+        percentage: 0.0,
+      },
+      week: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      month: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      ytd: {
+        priceDifference: 0,
+        percentage: 0,
+      },
+      year: {
+        priceDifference: 0,
+        percentage: 0
+      }
     },
   ];
 }
