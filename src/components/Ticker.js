ed110a65e9799c1eb8116abfdf96341cf3cc4d88
Michael McMasters <jmcmasters411@gmail.com>
2021-08-18 21:25:46 -0400

    Add blue bar to tickers that are under the dragged ticker

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { ColorThemeContext } from "./custom_hooks/ColorThemeContext";
 
 const Ticker = ({ tickerName, type, price, priceDifference, percentage }) => {
   const COLORS = useContext(ColorThemeContext);
+  const [display, setDisplay] = useState("block");
+  const [blueBarRight, setBlueBarRight] = useState(false);
 
   let bgColor, fontColor;
   if (priceDifference <= 0) {
@@ -15,8 +17,21 @@ const Ticker = ({ tickerName, type, price, priceDifference, percentage }) => {
     priceDifference = "+" + priceDifference;
   }
 
+  const handleOnClick = () => {
+    console.log("got click")
+    // setDisplay("none");
+  }
+
+  const handleDragOver = () => {
+    setBlueBarRight(true);
+  }
+
+  const handleDragLeave = () => {
+    setBlueBarRight(false);
+  }
+
   return (
-    <Container colors={COLORS} fontColor={fontColor} bgColor={bgColor} draggable="true">
+    <Container colors={COLORS} fontColor={fontColor} bgColor={bgColor} display={display} blueBarRight={blueBarRight} draggable="true" onMouseDown={handleOnClick} onDragOver={handleDragOver} onDragLeave={handleDragLeave}>
       <CoinTicker>{tickerName}</CoinTicker>
       <Price>${price}</Price>
       <PriceChange>
@@ -35,6 +50,9 @@ const Container = styled.div`
   color: ${(props) => props.fontColor};
   background-color: ${(props) => props.bgColor};
   text-align: center;
+  /* border-right: 1px solid blue; */
+  border-right: ${props => props.blueBarRight ? "4px solid blue" : ""};
+  display: ${props => props.display};
   cursor: move;
 `;
 
