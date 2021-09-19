f9fc8ccef749d850a58bfb7b6f1205c443b03ce4
Michael McMasters <jmcmasters411@gmail.com>
2021-09-19 14:58:55 -0400

    Refactor UseDragAndDrop hook to not use object

--- src/components/custom_hooks/UseDragAndDrop.js
+++ src/components/custom_hooks/UseDragAndDrop.js
@@ -1,97 +1,77 @@
 import { useEffect, useState } from 'react';
 
-const useDragAndDrop = (initialDragAndDropItems) => {
+const useDragAndDrop = (initialDragAndDropItems, initialAllowDragAndDrop = true) => {
 
-  const [dragAndDropItems, setDragAndDropItems] = useState(initialDragAndDropItems.map(item => {
-    return {
-      ...item,
-      beingDragged: false,
-      hitboxDetectingDraggedItem: false,
-      swapped: false
-    };
-  }));
+  const [dragAndDropItems, setDragAndDropItems] = useState(initialDragAndDropItems);
 
-
-  // Overrides set function to make sure objects have additional properties that this hook added.
-  const setDragAndDropItemsOverride = (newDragAndDropItems) => {
-    for (let i = 0; i < dragAndDropItems.length; i++) {
-      newDragAndDropItems[i].beingDragged = dragAndDropItems[i].beingDragged;
-      newDragAndDropItems[i].hitboxDetectingDraggedItem = dragAndDropItems[i].hitboxDetectingDraggedItem;
-      newDragAndDropItems[i].swapped = dragAndDropItems[i].swapped;
-    }
-    setDragAndDropItems(newDragAndDropItems);
-  }
+  const [allowDragAndDrop, setAllowDragAndDrop] = useState(false);
+  const [indexBeingDragged, setIndexBeingDragged] = useState(-1);
+  const [indexDetectingDraggedItem, setIndexDetectingDraggedItem] = useState(-1);
+  const [indexSwapped, setIndexSwapped] = useState([]);
 
 
   const swapItems = (itemsCopy, firstIndex, secondIndex) => {
     const firstItem = itemsCopy[firstIndex];
     itemsCopy[firstIndex] = itemsCopy[secondIndex];
     itemsCopy[secondIndex] = firstItem;
-    itemsCopy[firstIndex].swapped = true;
-    itemsCopy[secondIndex].swapped = true;
+    setIndexSwapped([firstIndex, secondIndex]);
     return itemsCopy;
   }
 
 
-  // Applies action to the item with the given index
   const handlers = {
 
+    setAllowDragAndDrop: setAllowDragAndDrop,
+
+    // The following functions applies actions to the item at the given index
+
     handleDragStart: (draggedItemIndex) => {
-      setDragAndDropItems(dragAndDropItems.map((item, ind) => {
-        if (ind === draggedItemIndex) {
-          item.beingDragged = true;
-        }
-        item.swapped = false;
-        return item;
-      }))
+      setIndexBeingDragged(draggedItemIndex);
+      setIndexSwapped([]);
     },
 
     handleDragEnd: (draggedItemIndex) => {
-      let itemsCopy = [...dragAndDropItems];
-      const hitboxDetectingIndex = itemsCopy.findIndex(item => item.hitboxDetectingDraggedItem == true);
-      if (hitboxDetectingIndex !== -1) {
-        itemsCopy = swapItems(itemsCopy, draggedItemIndex, hitboxDetectingIndex);
+      if (indexDetectingDraggedItem !== -1) {
+        setIndexDetectingDraggedItem(-1);
+        setIndexBeingDragged(-1);
+        swapItems([...dragAndDropItems], draggedItemIndex, indexDetectingDraggedItem);
       }
-      itemsCopy.forEach((item) => {
-        item.hitboxDetectingDraggedItem = false;
-        item.beingDragged = false;
-      });
-      setDragAndDropItems(itemsCopy);
     },
 
     handleHitboxEnter: (event, detectorIndex) => {
       event.preventDefault();
-      const itemsCopy = [...dragAndDropItems];
-      itemsCopy[detectorIndex].hitboxDetectingDraggedItem = true;
-      setDragAndDropItems(itemsCopy);
+      setIndexDetectingDraggedItem(detectorIndex);
     },
 
     handleHitboxLeave: (detectorIndex) => {
-      const itemsCopy = [...dragAndDropItems];
-      itemsCopy[detectorIndex].hitboxDetectingDraggedItem = false;
-      setDragAndDropItems(itemsCopy);
+      setIndexDetectingDraggedItem(-1);
     },
   }
 
 
-  // Gets status of the item at the given index
   const getters = {
 
+    getAllowDragAndDrop: () => {
+      return allowDragAndDrop;
+    },
+
+    // The following functions get the status of the item at the given index
+
     getBeingDragged: (index) => {
-      return dragAndDropItems[index].beingDragged;
+      return indexBeingDragged === index;
     },
 
     getHitboxDetectingDraggedItem: (index) => {
-      return dragAndDropItems[index].hitboxDetectingDraggedItem;
+      return indexDetectingDraggedItem === index;
     },
 
     getSwapped: (index) => {
-      return dragAndDropItems[index].swapped;
+      return indexSwapped.includes(index);
     },
   }
 
 
-  return [dragAndDropItems, setDragAndDropItemsOverride, handlers, getters];
+  return [dragAndDropItems, setDragAndDropItems, handlers, getters];
 };
 
 export default useDragAndDrop;
