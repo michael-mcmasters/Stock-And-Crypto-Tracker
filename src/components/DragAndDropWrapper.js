0be295dbd0cc80d834bc2b46d99c30723cdeca80
Michael McMasters <jmcmasters411@gmail.com>
2021-10-17 19:40:25 -0400

    Put container over all tickers

--- src/components/DragAndDropWrapper.js
+++ src/components/DragAndDropWrapper.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled, { css, keyframes } from "styled-components";
 
 const DragAndDropWrapper = ({dragAndDropHandlers, dragAndDropGetters, render}) => {
   
@@ -7,6 +8,10 @@ const DragAndDropWrapper = ({dragAndDropHandlers, dragAndDropGetters, render}) =
   const { setAllowDragAndDrop, handleDragStart, handleDragEnd, handleHitboxEnter, handleHitboxLeave } = dragAndDropHandlers;
   const { getAllowDragAndDrop, getBeingDragged, getHitboxDetectingDraggedItem, getSwapped } = dragAndDropGetters;
   
+  // let elements = [];
+  // for (let i = 0; i < tickers.length; i++) {
+  //   elements.push(<Container>tickers[0]</Container>)
+  // }
 
   return (
     <>
@@ -21,19 +26,45 @@ const DragAndDropWrapper = ({dragAndDropHandlers, dragAndDropGetters, render}) =
       
       {/* ToDo: Map over each element and wrap them with a draggable container that calls the above functions,
           gives them HitBox element, DropIndicator, etc. Tickers2 should only show the view. */}
-      {tickers[0]}
-      {tickers[1]}
-      {tickers[1]}
-      {tickers[1]}
-      {tickers[1]}
-      {tickers[1]}
-      {tickers[1]}
       
-      {/* {tickers.map(t => {
-        return t;
-      })} */}
+      
+      
+      {tickers.map((ticker, index) => (
+        <Container
+          draggable={getAllowDragAndDrop}
+          onDragStart={() => handleDragStart(index)}
+          onDragEnd={() => handleDragEnd(index)}
+          hitboxDetectingDraggedItem={getHitboxDetectingDraggedItem(index)}
+        >
+          {ticker}
+        </Container>
+      ))}
+    
     </>
   );
 };
+
+const FlashYellowAnimation = keyframes`
+  50% { background-color: yellow; }
+`;
+
+const Container = styled.div`
+  border: 2px solid ${(props) => props.fontColor};
+  padding: 0;
+  cursor: ${props => props.draggable ? "move" : ""};
+
+  ${props => props.beingDragged && css`
+    opacity: 0.3;
+  `}
+  
+  ${props => props.hitboxDetectingDraggedItem && css`
+    border: 2px solid yellow;
+  `}
+  
+  ${props => props.swapped == true && css`
+    animation-name: ${FlashYellowAnimation};
+    animation-duration: 0.8s;
+  `}
+`;
 
 export default DragAndDropWrapper;
